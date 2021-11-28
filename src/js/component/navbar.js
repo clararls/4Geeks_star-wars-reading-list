d1591b64a95640ad15288b46a16390c2079dc7ec
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-11-28 12:59:49 +0000

    cambios menores y color heading

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	// ver como poner el logo de star wars y intentar hacer un dropdown con un solo elemento
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span
+					src="https://logos-marcas.com/wp-content/uploads/2020/11/Star-Wars-Logo.png"
+					className="navbar-brand mb-0 h1">
+					React Boilerplate
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
