c85c3522c9d5b076874874245ce0f702312f7606
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-11-30 15:36:51 +0000

    headers aparte de las cards

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,24 +8,24 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 	return (
 		<div className="container testimonial-group">
+			<h1 className="categoryData">Characters</h1>
 			<div className="row">
-				<h1 className="categoryData">Characters</h1>
 				{store.characters
 					? store.characters.map((value, index) => {
 							return <Card key={index} value={value} index={index} category={"people"} />;
 					  })
 					: ""}
 			</div>
+			<h1 className="categoryData">Planets</h1>
 			<div className="row">
-				<h1 className="categoryData">Planets</h1>
 				{store.planets
 					? store.planets.map((value, index) => {
 							return <Card key={index} value={value} index={index} category={"planets"} />;
 					  })
 					: ""}
 			</div>
+			<h1 className="categoryData">Vehicles</h1>
 			<div className="row">
-				<h1 className="categoryData">Vehicles</h1>
 				{store.vehicles
 					? store.vehicles.map((value, index) => {
 							return <Card key={index} value={value} index={index} category={"vehicles"} />;
